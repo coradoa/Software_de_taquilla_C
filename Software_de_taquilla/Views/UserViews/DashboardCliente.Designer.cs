8d93bd2e2f4b6d39d6e795972c19122cca40640c
cchuye <carol.escobar.1116@gmail.com>
2023-07-20 10:40:38 -0600

    Menu
    Menu sin errores

--- Software_de_taquilla/Views/UserViews/DashboardCliente.Designer.cs
+++ Software_de_taquilla/Views/UserViews/DashboardCliente.Designer.cs
@@ -57,17 +57,20 @@
             PanelMenu.Dock = DockStyle.Left;
             PanelMenu.Location = new Point(0, 0);
             PanelMenu.Name = "PanelMenu";
-            PanelMenu.Size = new Size(250, 411);
+            PanelMenu.Size = new Size(227, 411);
             PanelMenu.TabIndex = 0;
             // 
             // btnSalir
             // 
             btnSalir.FlatAppearance.BorderSize = 0;
             btnSalir.FlatStyle = FlatStyle.Flat;
+            btnSalir.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnSalir.ForeColor = SystemColors.Control;
-            btnSalir.Location = new Point(157, 356);
+            btnSalir.Image = (Image)resources.GetObject("btnSalir.Image");
+            btnSalir.ImageAlign = ContentAlignment.MiddleRight;
+            btnSalir.Location = new Point(107, 351);
             btnSalir.Name = "btnSalir";
-            btnSalir.Size = new Size(75, 23);
+            btnSalir.Size = new Size(108, 27);
             btnSalir.TabIndex = 6;
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = true;
@@ -79,10 +82,13 @@
             btnMenuAyuda.FlatAppearance.MouseDownBackColor = Color.FromArgb(36, 171, 218);
             btnMenuAyuda.FlatAppearance.MouseOverBackColor = Color.FromArgb(36, 171, 218);
             btnMenuAyuda.FlatStyle = FlatStyle.Flat;
+            btnMenuAyuda.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnMenuAyuda.ForeColor = SystemColors.Control;
+            btnMenuAyuda.Image = (Image)resources.GetObject("btnMenuAyuda.Image");
+            btnMenuAyuda.ImageAlign = ContentAlignment.MiddleLeft;
             btnMenuAyuda.Location = new Point(0, 274);
             btnMenuAyuda.Name = "btnMenuAyuda";
-            btnMenuAyuda.Size = new Size(250, 45);
+            btnMenuAyuda.Size = new Size(227, 45);
             btnMenuAyuda.TabIndex = 5;
             btnMenuAyuda.Text = "Menú de ayuda";
             btnMenuAyuda.UseVisualStyleBackColor = true;
@@ -94,10 +100,13 @@
             btnCobros.FlatAppearance.MouseDownBackColor = Color.FromArgb(36, 171, 218);
             btnCobros.FlatAppearance.MouseOverBackColor = Color.FromArgb(36, 171, 218);
             btnCobros.FlatStyle = FlatStyle.Flat;
+            btnCobros.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnCobros.ForeColor = SystemColors.Control;
+            btnCobros.Image = (Image)resources.GetObject("btnCobros.Image");
+            btnCobros.ImageAlign = ContentAlignment.MiddleLeft;
             btnCobros.Location = new Point(0, 229);
             btnCobros.Name = "btnCobros";
-            btnCobros.Size = new Size(250, 45);
+            btnCobros.Size = new Size(227, 45);
             btnCobros.TabIndex = 4;
             btnCobros.Text = "Cobros";
             btnCobros.UseVisualStyleBackColor = true;
@@ -109,12 +118,13 @@
             btnCartelera.FlatAppearance.MouseDownBackColor = Color.FromArgb(36, 171, 218);
             btnCartelera.FlatAppearance.MouseOverBackColor = Color.FromArgb(36, 171, 218);
             btnCartelera.FlatStyle = FlatStyle.Flat;
+            btnCartelera.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnCartelera.ForeColor = SystemColors.Control;
             btnCartelera.Image = (Image)resources.GetObject("btnCartelera.Image");
             btnCartelera.ImageAlign = ContentAlignment.MiddleLeft;
             btnCartelera.Location = new Point(0, 184);
             btnCartelera.Name = "btnCartelera";
-            btnCartelera.Size = new Size(250, 45);
+            btnCartelera.Size = new Size(227, 45);
             btnCartelera.TabIndex = 3;
             btnCartelera.Text = "Cartelera";
             btnCartelera.UseVisualStyleBackColor = true;
@@ -126,10 +136,13 @@
             btnInicio.FlatAppearance.MouseDownBackColor = Color.FromArgb(36, 171, 218);
             btnInicio.FlatAppearance.MouseOverBackColor = Color.FromArgb(36, 171, 218);
             btnInicio.FlatStyle = FlatStyle.Flat;
+            btnInicio.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
             btnInicio.ForeColor = SystemColors.Control;
+            btnInicio.Image = (Image)resources.GetObject("btnInicio.Image");
+            btnInicio.ImageAlign = ContentAlignment.MiddleLeft;
             btnInicio.Location = new Point(0, 139);
             btnInicio.Name = "btnInicio";
-            btnInicio.Size = new Size(250, 45);
+            btnInicio.Size = new Size(227, 45);
             btnInicio.TabIndex = 2;
             btnInicio.Text = "Inicio";
             btnInicio.UseVisualStyleBackColor = true;
@@ -143,17 +156,17 @@
             PanelDatos.Dock = DockStyle.Top;
             PanelDatos.Location = new Point(0, 0);
             PanelDatos.Name = "PanelDatos";
-            PanelDatos.Size = new Size(250, 139);
+            PanelDatos.Size = new Size(227, 139);
             PanelDatos.TabIndex = 1;
             // 
             // lblEstado
             // 
             lblEstado.AutoSize = true;
-            lblEstado.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblEstado.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             lblEstado.ForeColor = Color.FromArgb(43, 105, 130);
             lblEstado.Location = new Point(124, 99);
             lblEstado.Name = "lblEstado";
-            lblEstado.Size = new Size(39, 16);
+            lblEstado.Size = new Size(44, 15);
             lblEstado.TabIndex = 3;
             lblEstado.Text = "Estado";
             lblEstado.Click += label3_Click;
@@ -161,11 +174,11 @@
             // lblPerfil
             // 
             lblPerfil.AutoSize = true;
-            lblPerfil.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPerfil.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             lblPerfil.ForeColor = SystemColors.Control;
             lblPerfil.Location = new Point(124, 74);
             lblPerfil.Name = "lblPerfil";
-            lblPerfil.Size = new Size(44, 16);
+            lblPerfil.Size = new Size(49, 15);
             lblPerfil.TabIndex = 2;
             lblPerfil.Text = "Usuario";
             lblPerfil.Click += label2_Click;
@@ -174,7 +187,7 @@
             // 
             PctPerfil.Location = new Point(49, 65);
             PctPerfil.Name = "PctPerfil";
-            PctPerfil.Size = new Size(66, 50);
+            PctPerfil.Size = new Size(66, 57);
             PctPerfil.TabIndex = 1;
             PctPerfil.TabStop = false;
             // 
@@ -183,7 +196,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Bookman Old Style", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(49, 26);
+            label1.Location = new Point(35, 25);
             label1.Name = "label1";
             label1.Size = new Size(152, 19);
             label1.TabIndex = 0;
