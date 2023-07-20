e4696d5e4ab6f1603c9fab1198c5effcb80d6232
Olstertecn11 <otzunund@miumg.edu.gt>
2023-07-19 22:29:41 -0600

    Create Movie.cs

---
+++ Software_de_taquilla/Models/Dto/Movie.cs
@@ -0,0 +1,29 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_de_taquilla.Models.Dto
+{
+    public class Movie
+    {
+        public int id;
+        public string name;
+        public string image;
+        public int duration;
+        public int id_listing;
+        public int id_room;
+
+        public Movie(int id, string name, string image, int duration, int id_listing, int id_room)
+        {
+            this.id = id;
+            this.name = name;
+            this.image = image;
+            this.duration = duration;
+            this.id_listing = id_listing;
+            this.id_room = id_room;
+        }
+    }
+}
