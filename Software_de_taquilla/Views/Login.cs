418bceed2e76263fc08f83b5dd9c0057d26fc8b8
Olstertecn11 <otzunund@miumg.edu.gt>
2023-07-24 19:37:58 -0600

    quitando efectos
    antesde quitar efectos

--- Software_de_taquilla/Views/Login.cs
+++ Software_de_taquilla/Views/Login.cs
@@ -18,57 +18,35 @@ namespace Software_de_taquilla.Views
             InitializeComponent();
             LoginController usr = new LoginController(this);
         }
-
-        private void Login_Load(object sender, EventArgs e)
-        {
-
-        }
-
         public void printMessage(string message)
         {
             MessageBox.Show(message);
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txt_user_Click(object sender, EventArgs e)
+        private void txt_user_MouseClick(object sender, MouseEventArgs e)
         {
             txt_user.Text = "";
             txt_user.ForeColor = Color.Black;
         }
 
-        private void txt_user_Leave(object sender, EventArgs e)
+        private void txt_user_MouseLeave(object sender, EventArgs e)
         {
             txt_user.Text = "Usuario";
             txt_user.ForeColor = SystemColors.WindowFrame;
+
         }
 
-        private void txt_pass_Click_1(object sender, EventArgs e)
+        private void txt_pass_MouseClick(object sender, MouseEventArgs e)
         {
             txt_pass.Text = "";
             txt_pass.ForeColor = Color.Black;
             txt_pass.PasswordChar = '*';
-
         }
 
-        private void txt_pass_Leave_1(object sender, EventArgs e)
+        private void txt_pass_MouseLeave(object sender, EventArgs e)
         {
             txt_pass.Text = "Contraseña";
             txt_pass.ForeColor = SystemColors.WindowFrame;
             txt_pass.PasswordChar = '\0';
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
